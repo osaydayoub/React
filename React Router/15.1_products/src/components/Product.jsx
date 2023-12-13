f9a50cd2f01fe80f1aede60f5a6c847dde7975fa
osaydayoub <osaydayoub@gmail.com>
2023-12-13 13:41:15 +0200

    15.1 products

--- React Router/15.1_products/src/components/Product.jsx
+++ React Router/15.1_products/src/components/Product.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import { Link, useLocation, useParams } from 'react-router-dom'
 
 function Product() {
-    console.log(useParams())
-    const { id } = useParams()
     const location = useLocation()
-    console.log(location.state);
     return (
         <div className='newProduct-container products-page'>
             <h1>{location.state.product.title}</h1>
